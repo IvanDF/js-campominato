bf38512179bece55dba4e84b18ae3f503489b641
Ivan <delfatti.ivan@gmail.com>
2020-10-23 15:45:18 +0200

    Da riordinare

--- js/rule.js
+++ js/rule.js
@@ -2,47 +2,67 @@
  *  PRATO FIORITO
  *********************/
 
-// ELEMENTI
+document.getElementById('bottone').addEventListener('click', function(){
 
-// BOMBE
-var bombeTot = []
-var numBombe = 16
+    // SELEZIONE DIFFICOLTÁ 
+    var diff = 0;
+    var numMax = 0;
 
+    var livelloScelto = document.getElementById('difficolta').value
+    console.log(livelloScelto);
 
+    switch(livelloScelto) {
+        case 'facile':
+            diff = 20 - numBombe;
+            numMax = 100;
+            break
+        case 'normale':
+            diff = 24 - numBombe;
+            numMax = 80;
+            break
+        case 'difficile':
+            diff = 28 - numBombe;
+            numMax = 50;
+            break
+        default:
+            'none'
+    }
 
-// GENERAZIONE BOMBE
-while (bombeTot.length < numBombe) {
-    var bomba = genBombe(1, 100);
+    // BOMBE
+    var bombeTot = []
+    var numBombe = 16
+
+    // GENERAZIONE BOMBE
+    while (bombeTot.length < numBombe) {
+        var bomba = genBombe(1, numMax);
+        
+        if (! bombeTot.includes(bomba) ) {
+            bombeTot.push(bomba);
+        }
+        
+    }
+
+    console.log(bombeTot);
+    console.log(diff);
+
+    for ( i = 0; i < diff; i++ ) {
+        var numUtente = parseInt( prompt( 'Inserisci un numero da 1 a ' + numMax ) );
+        while ( (isNaN(numUtente) ) || (numUtente <= 0) || (numUtente > 100) )  {
+            numUtente = parseInt( prompt( 'Non hai inserito un carattere valido, prego inserire un numero da 1 a 100' ) );
+        }
     
-    if (! bombeTot.includes(bomba) ) {
-        bombeTot.push(bomba);
+        var check = bombeTot.includes(numUtente);
+        if ( check == true ) {
+            alert('hai perso');
+            break;
+        }
     }
-    
-}
-
-console.log(bombeTot);
-
-
-// UTENTE
-var difficolta = 10
-
-for ( i = 0; i < difficolta; i++ ) {
-    var numUtente = parseInt( prompt( 'Inserisci un numero da 1 a 100' ) );
-    while ( (isNaN(numUtente) ) || (numUtente <= 0) || (numUtente > 100) )  {
-        numUtente = parseInt( prompt( 'Non hai inserito un carattere valido, prego inserire un numero da 1 a 100' ) );
+    if (i === diff) {
+        alert('HAI VINTO')
+        console.log('hai vinto');
     }
-
-    if ( !bombeTot[numUtente] ) {
-        alert('Hai perso!')
-        break;
-    }
-}
-
-if (i === difficolta) {
-    alert('HAI VINTO')
-}
-console.log( 'Tentativi: ' + i);
-
+    console.log( 'Tentativi: ' + i);
+})
 
 
 // FUNZIONE GENERAZIONE NUMERI CASUALI
